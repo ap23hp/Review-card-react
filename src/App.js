b51303f564bd6f677e2c69cfbef170702ea72fa6
Apra Khanna <67959896+ap23hp@users.noreply.github.com>
2022-04-12 13:52:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Review from './Review';
+//npm install react-icons --save
+//import {FaGithubSquare} from 'react-icons/fa'
+function App() {
+  return (
+<>
+
+<section className='container'>
+<div className='title'>
+  <h2>Our Reviews</h2>
+
+<div className="underline"> 
+
+</div>
+</div>
+<Review/>
+</section>
+
+</>
+
+
+  )
+  
+  
+}
+
+export default App;
